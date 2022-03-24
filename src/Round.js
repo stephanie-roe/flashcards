36dbbbb08cb15848d8c88129ec86ed699cbf975f
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-03-23 20:08:58 -0600

    Add all functionalities to the Round class

---
+++ src/Round.js
@@ -0,0 +1,33 @@
+const Turn = require('../src/Turn');
+
+
+class Round {
+  constructor(deck) {
+    this.deck = deck,
+    this.turns = 0
+    this.incorrectGuesses = []
+  }
+  returnCurrentCard() {
+    return this.deck.cards[this.turns]
+  }
+  takeTurn(guess) {
+    const currentCard = this.returnCurrentCard()
+    const turn = new Turn (guess, currentCard)
+    this.turns ++
+    turn.evaluateGuess()
+    if (turn.isCorrect === false) {
+      this.incorrectGuesses.push(currentCard.id)
+    }
+    return turn.giveFeedback()
+  }
+  calculatePercentCorrect() {
+    const percentage = ((this.turns - this.incorrectGuesses.length) / this.turns) * 100
+    return percentage
+  }
+  endRound() {
+    return `Round over! You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+  }
+}
+
+
+module.exports = Round
